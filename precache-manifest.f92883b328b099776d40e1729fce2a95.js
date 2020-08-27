8777adda07de0deefb0fddbe2265683c24e41269
Karla Rodrigues <karlaadautor@gmail.com>
2020-08-27 18:39:02 -0400

    Updates

---
+++ precache-manifest.f92883b328b099776d40e1729fce2a95.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5013df4f60b0ca9c61b79166e19713",
+    "url": "/MangiareApp/index.html"
+  },
+  {
+    "revision": "39fc0981d3ce75c6b63d",
+    "url": "/MangiareApp/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "1083879ee31ef052c584",
+    "url": "/MangiareApp/static/css/main.149b8bd9.chunk.css"
+  },
+  {
+    "revision": "39fc0981d3ce75c6b63d",
+    "url": "/MangiareApp/static/js/2.dcacffae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MangiareApp/static/js/2.dcacffae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1083879ee31ef052c584",
+    "url": "/MangiareApp/static/js/main.af1706b4.chunk.js"
+  },
+  {
+    "revision": "a36bfdacbf3773002d65",
+    "url": "/MangiareApp/static/js/runtime-main.993917b5.js"
+  },
+  {
+    "revision": "aee563e284b7087d2c81e152daef796d",
+    "url": "/MangiareApp/static/media/Food01.aee563e2.jpg"
+  },
+  {
+    "revision": "b6a00b727f50d44358815a33fcbb3428",
+    "url": "/MangiareApp/static/media/Food02.b6a00b72.jpg"
+  },
+  {
+    "revision": "5a94cd417704546946abaaa707f55e7e",
+    "url": "/MangiareApp/static/media/Food03.5a94cd41.jpg"
+  },
+  {
+    "revision": "efcc5fc030313974f76383bd156aa40b",
+    "url": "/MangiareApp/static/media/Food04.efcc5fc0.jpg"
+  },
+  {
+    "revision": "1a75a56422847cc2add68fa046e07ab7",
+    "url": "/MangiareApp/static/media/Food05.1a75a564.jpg"
+  }
+]);
